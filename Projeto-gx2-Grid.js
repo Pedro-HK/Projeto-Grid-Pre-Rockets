daa00b548e676cfd5bb14a2f25d10d2970d4859c
Pedro Knorst <phknorst@gmail.com>
2023-04-19 20:11:05 -0300

    commit-2

--- Projeto-gx2-Grid.js
+++ Projeto-gx2-Grid.js
@@ -1,22 +1,26 @@
-let itemsSelecionado = 0
+(function(){
+    const $carroselDeImagens = document.getElementById('carrosel-de-imagens')
 
-const items = document.querySelectorAll('.item')
-
-function controle (){
-
-    itemsSelecionado++
+    let itemsSelecionado = 0
     
-    if(itemsSelecionado >= items.length){
-        itemsSelecionado = 0
-    }
-
-    items.forEach(item => item.classList.remove('item-selecionado'))
-
-    items[itemsSelecionado].scrollIntoView({
-        inline: "center",
-        behavior: "smooth",
-        block:"nearest"
+    const items = document.querySelectorAll('.item')
+    
+    $carroselDeImagens.addEventListener('click', function (){
+    
+        itemsSelecionado++
+        
+        if(itemsSelecionado >= items.length){
+            itemsSelecionado = 0
+        }
+    
+        items.forEach(item => item.classList.remove('item-selecionado'))
+    
+        items[itemsSelecionado].scrollIntoView({
+            inline: "center",
+            behavior: "smooth",
+            block:"nearest"
+        })
+    
+        items[itemsSelecionado].classList.add('item-selecionado')
     })
-
-    items[itemsSelecionado].classList.add('item-selecionado')
-}
+})()
